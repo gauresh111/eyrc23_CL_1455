1302459ba91265869f29a1f42e79f3232e6054c6
gauresh111 <gaureshwadekar92@student.sfit.ac.in>
2023-12-02 17:05:47 +0530

    Update config.yaml and nav2_params.yaml

--- mani_stack/launch/task3b.launch.py
+++ mani_stack/launch/task3b.launch.py
@@ -45,10 +45,10 @@ def generate_launch_description():
         ]],
         shell=True
     )
-    star_ServoManipulation = ExecuteProcess(
-    cmd=[['ros2 run mani_stack servoManipulation_nav2.py']],
-    shell=True
-    )
+    # star_ServoManipulation = ExecuteProcess(
+    # cmd=[['ros2 run mani_stack servoManipulation_nav2.py']],
+    # shell=True
+    # )
     start_yaml_controller = ExecuteProcess(
         cmd=[[
             'ros2 run ebot_docking ebot_nav2_yaml.py',
