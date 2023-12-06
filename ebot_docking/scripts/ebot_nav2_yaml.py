99ef73a90534d376f58af7462b48c33f8f9d5d86
gauresh111 <gaureshwadekar92@student.sfit.ac.in>
2023-12-07 01:00:00 +0530

    Refactor ebot_nav2_yaml.py script

--- ebot_docking/scripts/ebot_nav2_yaml.py
+++ ebot_docking/scripts/ebot_nav2_yaml.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python3
 import os
-from nav_msgs.msg import Odometry
 import rclpy
 from threading import Thread
 import time
-from geometry_msgs.msg import PoseStamped
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.node import Node
-from nav2_simple_commander.robot_navigator import BasicNavigator
-import tf2_ros
-from rclpy.duration import Duration # Handles time for ROS 2
 from scipy.spatial.transform import Rotation as R
 from std_msgs.msg import Bool
 from ebot_docking.srv import RackSw  # Import custom service message
-from tf_transformations import euler_from_quaternion
 import math
-from rcl_interfaces.srv import SetParameters
-from geometry_msgs.msg import Polygon,Point32
 import yaml
 from ament_index_python.packages import get_package_share_directory
-from std_msgs.msg import String
-from std_msgs.msg import Bool
 config_folder_name = 'ebot_docking'
 
 global dockingPosition
