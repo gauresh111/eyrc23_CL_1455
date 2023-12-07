35f9c32ecd193f3eaefddc8755e8d767a39e9a89
gauresh111 <gaureshwadekar92@student.sfit.ac.in>
2023-12-08 01:28:38 +0530

    Add exitNav2.py script and update task3b.launch.py

--- mani_stack/launch/task3b.launch.py
+++ mani_stack/launch/task3b.launch.py
@@ -21,8 +21,15 @@ def generate_launch_description():
         ]],
         shell=True
     )
+    Start_exitNav = ExecuteProcess(
+        cmd=[[
+            'ros2 run mani_stack exitNav2.py',
+        ]],
+        shell=True
+    )
     return LaunchDescription([
-     start_docking,
-     start_yaml_controller,
-     start_nav2
+    start_docking,
+    start_yaml_controller,
+    start_nav2,
+    Start_exitNav
     ])
--- mani_stack/scripts/exitNav2.py
+++ mani_stack/scripts/exitNav2.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
+import re
 from threading import Thread
 import time
+
+import psutil
 import rclpy
 from rclpy.node import Node
 import subprocess
 import signal
 from std_msgs.msg import Bool
 import os
-import pyproc2
+from subprocess import check_output, CalledProcessError
 def main():
     rclpy.init()
     node = Node("exitNav2")
@@ -15,14 +18,32 @@ def main():
     executor.add_node(node)
     executor_thread = Thread(target=executor.spin, daemon=True, args=())
     executor_thread.start()
+    def get_pid(name):
+        result = check_output(["pgrep","-af",name]).decode('ASCII')
+        pid_list=re.findall(r'\b\d+\b', result)
+        return pid_list
+    def kill_process():
+        programName = ["nav2_cmd.py","ebot_nav2_yaml.py","ebot_docking_boilerplate.py","task3b.launch.py","SpwanNav2.launch.py","ekf_node","component_container_isolated","exitNav2.py"]    
+        for i in range(len(programName)):
+            program_name = programName[i]
+            pid = get_pid(program_name)
+            if pid:
+                print("pid",pid)
+                for j in range(len(pid)):
+                    try:
+                        
+                        os.kill(int(pid[j]), signal.SIGTERM)
+                        print("killed",pid[j],program_name)
+                    except OSError as e:
+                        print("No process running with name: ", program_name)
+            else:
+                print("No process running with name: ", program_name)
     def Exitcallback(msg):
         if msg.data:
             print("Exitcallback",msg.data)
+            kill_process()
     node.Exit = node.create_subscription(Bool, '/ExitNav',Exitcallback, 30)
-    eyantra_Wahrehouse_pid=pyproc2.find("task3a.launch.py").pid 
-    print(eyantra_Wahrehouse_pid)
     rclpy.spin(node)
-   
     node.destroy_node()
     rclpy.shutdown()
     exit(0)
