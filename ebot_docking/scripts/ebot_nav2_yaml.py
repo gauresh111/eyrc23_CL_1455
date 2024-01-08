02432ed8829c98a66aa895e8b183f774f6da81b8
gauresh111 <gaureshwadekar92@student.sfit.ac.in>
2024-01-09 03:13:08 +0530

    Remove package ID from list

--- ebot_docking/scripts/ebot_nav2_yaml.py
+++ ebot_docking/scripts/ebot_nav2_yaml.py
@@ -132,6 +132,7 @@ def main():
             node.ArmManipulationRequest.starting = True
             print("going to racks",node.ArmManipulationRequest)
             del dockingPosition[boxPresent] 
+            package_id.remove(int(boxPresent[-1]))
             futureArm = node.ArmManipulationClient.call_async(node.ArmManipulationRequest)
     def distance(p1, p2):
         return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
