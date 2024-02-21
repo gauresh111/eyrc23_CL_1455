66a50620020c54e9948bbadb11bb2321e20a9ecb
Joel Devasia <joeldevasia1962002@gmail.com>
2024-02-21 21:15:53 +0530

    Add shebang and parameter declaration in switchController.py

--- mani_stack/scripts/armManipulation/switchController.py
+++ mani_stack/scripts/armManipulation/switchController.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from controller_manager_msgs.srv import SwitchController
 import time
 import rclpy
@@ -5,35 +7,45 @@ from rclpy.node import Node
 def main():
     rclpy.init()
     node = Node("controller_manager_switcher")
-    def switch_controller(useMoveit: bool):
-        contolMSwitch = node.create_client(
-            SwitchController, "/controller_manager/switch_controller"
-        )
-        # Parameters to switch controller
-        switchParam = SwitchController.Request()
-        if useMoveit == True:
-            switchParam.activate_controllers = [
-                "scaled_joint_trajectory_controller"
-            ]  # for normal use of moveit
-            switchParam.deactivate_controllers = ["forward_position_controller"]
-        else:
-            switchParam.activate_controllers = [
-                "forward_position_controller"
-            ]  # for servoing
-            switchParam.deactivate_controllers = [
-                "scaled_joint_trajectory_controller"
-            ]
-        switchParam.strictness = 2
-        switchParam.start_asap = False
+    node.declare_parameter(
+        "useMoveit", True,
+    )
+    useMoveit = (
+        node.get_parameter("useMoveit").get_parameter_value().bool_value
+    )
+    print("Switching to", "Moveit" if useMoveit else "Servo")
 
-        # calling control manager service after checking its availability
-        while not contolMSwitch.wait_for_service(timeout_sec=5.0):
-            node.get_logger().warn(
-                f"Service control Manager is not yet available..."
-            )
-        contolMSwitch.call_async(switchParam)
-        time.sleep(1.0)
-        print(
-            "[CM]: Switching to", "Moveit" if useMoveit else "Servo", "Complete"
+    contolMSwitch = node.create_client(
+        SwitchController, "/controller_manager/switch_controller"
+    )
+    # Parameters to switch controller
+    switchParam = SwitchController.Request()
+    if useMoveit == True:
+        switchParam.activate_controllers = [
+            "scaled_joint_trajectory_controller"
+        ]  # for normal use of moveit
+        switchParam.deactivate_controllers = ["forward_position_controller"]
+    else:
+        switchParam.activate_controllers = [
+            "forward_position_controller"
+        ]  # for servoing
+        switchParam.deactivate_controllers = [
+            "scaled_joint_trajectory_controller"
+        ]
+    switchParam.strictness = 2
+    switchParam.start_asap = False
+
+    # calling control manager service after checking its availability
+    while not contolMSwitch.wait_for_service(timeout_sec=5.0):
+        node.get_logger().warn(
+            f"Service control Manager is not yet available..."
         )
-    
+    contolMSwitch.call_async(switchParam)
+    time.sleep(1.0)
+    print(
+        "[CM]: Switching to", "Moveit" if useMoveit else "Servo", "Complete"
+    )
+        
+if __name__ == "__main__":
+    main()
+        
