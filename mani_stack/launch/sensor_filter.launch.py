7e2ca307908df1aa738f931d32d56b860457defb
gauresh111 <gaureshwadekar92@student.sfit.ac.in>
2024-02-27 19:51:34 +0530

    Add launch file for sensor filter

--- mani_stack/launch/sensor_filter.launch.py
+++ mani_stack/launch/sensor_filter.launch.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python3
+'''
+# Team ID:          < 1455 >
+# Theme:            < Cosmo Logistic >
+# Author List:      < Joel Devasia , Gauresh Wadekar >
+# Filename:         < sensor_filter.launch.py >
+# Functions:        < generate_launch_description >
+# Global variables: < none >
+'''
 
 from launch.actions import ExecuteProcess
 from launch import LaunchDescription
 def generate_launch_description():
-
+    '''
+    Purpose: to launch the perception.py, duplicateImu.py and ultraFilter.py
+    args: None
+    return: LaunchDescription
+    '''
     
     
     Start_perceiption = ExecuteProcess( 
